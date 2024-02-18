f9bf34e15b798cd89496f0be16922df3c2505201
Martyna Zając <martyna.sabina.zajac@gmail.com>
2024-02-18 16:51:19 +0100

    wprowadzono zmiany na podział wydatku nazwa, typ, kwota, ww tym celu zastosowano krotkę

--- sledzenie_wydatkow.py
+++ sledzenie_wydatkow.py
@@ -1,35 +1,27 @@
 months={1:'Styczeń', 2:'Luty', 3:'Marzec', 4:'Kwiecień', 5:'Maj', 6:'Czerwiec', 7:'Lipiec', 8:'Sierpień', 9:'Wrzesień', 10: 'Październik', 11:'Listopad', 12: 'Grudzień'}
-date={1:{'rachunki': 220,'jedzonko dla psów': 250}, 2:{'rachunki': 150, 'jedzonko dla psów': 130, 'wycieczka': 2500 }}
+# date={1:{'rachunki': 220,'jedzonko dla psów': 250}, 2:{'rachunki': 150, 'jedzonko dla psów': 130, 'wycieczka': 2500 }}
 
+all_expenses={1:[("prąd", "rachunki", 250), ("karma sucha", "wydatki na pieski", 150)]}
+expenses_data=("expense_name", "expense_type", "expense_cost")
 
 def expand_list(choosen_month):
     print('--------------------------------')
     print('Twoje wydatki:')
-    for key,value in choosen_month.items():
-        print(f'{key}:{value}')
+    for date in choosen_month:
+        print(f'{expenses_data[0]}:{date[0]}, {expenses_data[1]}:{date[1]}, {expenses_data[2]}:{date[2]}')
 
-def add_expenses(choosen_month):
-    new_expenses=str(input('Wprowadź nazwę nowego wydatku: '))
-    while True:
-        try:
-            cost=int(input('Wprowadź kwotę nowego wydatku: '))
-            choosen_month[new_expenses]=cost
-            break
-        except ValueError:
-            print('Podana wartość nie jest liczbą')
+def add_expenses(choosen_month, add_new_expenses):
+    choosen_month.append(add_new_expenses)
 
-def delete_expenses(choosen_month):
-        delete_expenses=str(input('Wpisz nazwę wydatku jaki ma zostać usuniety z listy dla wybranego miesiąca: '))
-        if delete_expenses in choosen_month:
-            choosen_month.pop(delete_expenses)
-        else:
-            print('Brak podanego wydatku.')
+def delete_expenses(choosen_month, index_of_the_delete_expense):
+    choosen_month.pop(index_of_the_delete_expense)
+
 
 
 def expenses(month, month_name, expenses_list):
     if month not in expenses_list:
         print('Dla wskazanego miesiąca nie zostały wprowadzone dane. Lista jest pusta. W dalszych krokach możesz dodać wydatki.')
-        expenses_list[month]={}
+        expenses_list[month]=[]
     choosen_month=expenses_list[month]
     while True:
         try:
@@ -41,9 +33,27 @@ def expenses(month, month_name, expenses_list):
             elif menu_list == 1:
                 expand_list(choosen_month)
             elif menu_list==2:
-                add_expenses(choosen_month)
+                expense_name=input('Wprowadź nazwę nowego wydatku: ')
+                expense_type=input('Wprowadź typ nowego wydatku: ')
+                while True:
+                    try:
+                        expense_cost=int(input('Wprowadź kwotę nowego wydatku: '))
+                        break
+                    except ValueError:
+                        print('Podana niepoprawną wartość dla kwoty.')
+                add_new_expenses=(expense_name, expense_type, expense_cost)
+                add_expenses(choosen_month, add_new_expenses)
             elif menu_list == 3:
-                delete_expenses(choosen_month)
+                expense_name=str(input('Wpisz nazwę wydatku jaki ma zostać usuniety z listy dla wybranego miesiąca: '))
+                expense_found = False
+                for delete_expense in choosen_month:
+                    if expense_name == delete_expense[0]:
+                        index_of_the_delete_expense = choosen_month.index(delete_expense)
+                        print(choosen_month[index_of_the_delete_expense])
+                        delete_expenses(choosen_month, index_of_the_delete_expense)
+                        expense_found = True
+                if not expense_found:
+                    print('Brak podanego wydatku.')     
             else:
                 print('Wybrano wartość z pozalisty. Wybierz ponownie.')
         except ValueError:
@@ -61,7 +71,7 @@ while True:
         else:
             month_name=months[month]
             print(f'Wybrałeś miesiąc: {month_name}')
-            expenses(month, month_name, date)
+            expenses(month, month_name, all_expenses)
     except ValueError:
         print('Wprowadziłeś nie poprawne dane. Wprowadzone dane powinny być liczbą.')
 
